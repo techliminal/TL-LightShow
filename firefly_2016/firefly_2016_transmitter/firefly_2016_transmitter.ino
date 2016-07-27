36f4f2e04faccad6d80289cc10d04093f1e4d53c
ammist <schreck@SchreckBook.local>
2016-07-26 22:05:09 -0700

    first pass at transmitting apple codes. One works! (o)

--- firefly_2016/firefly_2016_transmitter/firefly_2016_transmitter.ino
+++ firefly_2016/firefly_2016_transmitter/firefly_2016_transmitter.ino
@@ -3,12 +3,33 @@
  * 
  * (C)2016 Anca Mosoiu and Michael Schrecker, Garaj Luminat
  * Source: https://learn.adafruit.com/using-an-infrared-library/sending-ir-codes
- *
+ * IRLib Documentation: http://tech.cyborg5.com/irlib/docs/
+ * IR codes: https://hifiduino.wordpress.com/apple-aluminum-remote/
  */
 
 #include <IRremote.h>
 
+enum {
+    CMD_LEN = 32,
+    UP      = 0x77E15061,
+    DOWN    = 0x77E13061,
+    PLAY    = 0x77E1A05E,
+    PREV    = 0x77E1905E,
+    NEXT    = 0x77E1605E,
+    MENU    = 0x77E1C05E
+  };
+
+
+
 IRsend irsend;
+
+void send_command(long command){
+    int i;
+    for (i=0; i<10; i++){
+      irsend.sendNEC(PLAY, 32);
+    } 
+    delay(20);
+}
 
 void setup()
 {
@@ -16,10 +37,19 @@ void setup()
 }
 
 void loop() {
-  if (Serial.read() != -1) {
-    for (int i = 0; i < 10; i++) {
-      irsend.sendSony(0xa90, 12); // Sony TV power code
-      delay(40);
-    }
+  const char command = Serial.read();
+  switch(command) {
+    case 'p':
+     send_command(UP); // all blue
+     break;
+    case 'o':
+      send_command(PLAY);
+      break;    
+    case 'i':
+      send_command(PREV);
+      break;
+    case 'u':
+      send_command(NEXT);
+      break;
   }
 }
