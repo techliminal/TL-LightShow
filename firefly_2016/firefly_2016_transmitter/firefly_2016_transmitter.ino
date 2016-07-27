9602c8c50f77e322bf768a33d2ec22bc48b76c79
ammist <schreck@SchreckBook.local>
2016-07-26 21:14:56 -0700

    first working ir transmitter

--- firefly_2016/firefly_2016_transmitter/firefly_2016_transmitter.ino
+++ firefly_2016/firefly_2016_transmitter/firefly_2016_transmitter.ino
@@ -2,6 +2,7 @@
  * firefly_2016_transmitter: Send IR Codes to the fireflies.
  * 
  * (C)2016 Anca Mosoiu and Michael Schrecker, Garaj Luminat
+ * Source: https://learn.adafruit.com/using-an-infrared-library/sending-ir-codes
  *
  */
 
@@ -16,7 +17,7 @@ void setup()
 
 void loop() {
   if (Serial.read() != -1) {
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 10; i++) {
       irsend.sendSony(0xa90, 12); // Sony TV power code
       delay(40);
     }
